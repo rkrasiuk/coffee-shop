83b5c827893d4b5773554b0a0e46edd2a2c37427
Roman <rokrassyuk@gmail.com>
2018-04-29 18:34:56 +0300

    initial

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const MAIN = '/main';
+export const MENU = '/menu';
+export const COUPONS = '/coupons';
+export const ORDER = '/order';
